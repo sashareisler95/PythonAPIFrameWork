e4dfb13779e0ff3f381173877b277bcbebe83b50
Александра Патошкина <APatoshkina@croc.ru>
2023-10-29 20:17:52 +0300

    create ex19

--- tests/test_user_delete.py
+++ tests/test_user_delete.py
@@ -26,35 +26,39 @@ class TestUserDelete(BaseCase):
     @allure.description('This test verifies that the user was deleted correctly')
     def test_delete_user_successfully(self):
         # REGISTER
-        register_data = self.prepare_registration_data()
-        response = MyRequests.post("/user/", data=register_data)
-        Assertions.assert_status_code(response, 200)
-        Assertions.assert_json_has_key(response, 'id')
-        email = register_data['email']
-        password = register_data['password']
-        user_id = self.get_json_value(response, 'id')
+        with allure.step('Регистрация нового пользователя'):
+            register_data = self.prepare_registration_data()
+            response = MyRequests.post("/user/", data=register_data)
+            Assertions.assert_status_code(response, 200)
+            Assertions.assert_json_has_key(response, 'id')
+            email = register_data['email']
+            password = register_data['password']
+            user_id = self.get_json_value(response, 'id')
 
         # LOGIN
-        login_data = {
-            'email': email,
-            'password': password
-        }
-        response = MyRequests.post('/user/login', data=login_data)
-        Assertions.assert_status_code(response, 200)
-        auth_sid = self.get_cookie(response, 'auth_sid')
-        token = self.get_header(response, 'x-csrf-token')
+        with allure.step('Авторизация под зарегистрированным пользователем'):
+            login_data = {
+                'email': email,
+                'password': password
+            }
+            response = MyRequests.post('/user/login', data=login_data)
+            Assertions.assert_status_code(response, 200)
+            auth_sid = self.get_cookie(response, 'auth_sid')
+            token = self.get_header(response, 'x-csrf-token')
 
         # DELETE
-        response = MyRequests.delete(f"/user/{user_id}", headers={'x-csrf-token': token},
-                                     cookies={'auth_sid': auth_sid})
-        Assertions.assert_status_code(response, 200)
+        with allure.step('Удаление пользователя, из под которого авторизованны'):
+            response = MyRequests.delete(f"/user/{user_id}", headers={'x-csrf-token': token},
+                                         cookies={'auth_sid': auth_sid})
+            Assertions.assert_status_code(response, 200)
 
         # GET USER DATA
-        response = MyRequests.get(f"/user/{user_id}",
-                                  headers={'x-csrf-token': token},
-                                  cookies={'auth_sid': auth_sid})
-        Assertions.assert_status_code(response, 404)
-        assert response.text == 'User not found', 'The user was not deleted'
+        with allure.step('Получаем ответ, получилось ли удалить пользователя'):
+            response = MyRequests.get(f"/user/{user_id}",
+                                      headers={'x-csrf-token': token},
+                                      cookies={'auth_sid': auth_sid})
+            Assertions.assert_status_code(response, 404)
+            assert response.text == 'User not found', 'The user was not deleted'
 
     @allure.description('This test checks for user deletion when the deletor is authorized with different credentials')
     def test_delete_user_by_another_user(self):
